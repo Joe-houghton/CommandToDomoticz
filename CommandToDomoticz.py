afb049ff2a0187396cc29d1a4e9be44f90ca460d
Joe-houghton <Joe_houghton@live.co.uk>
2018-06-25 19:25:31 +0100

    Bugs Fixed and we're ready to go!
    Can also run separately

--- CommandToDomoticz.py
+++ CommandToDomoticz.py
@@ -6,31 +6,22 @@ import requests
 
 from collections import namedtuple
 
+DomoticzDevice = namedtuple("DomoticzDevice", "name idx type")
 
-# CONFIG START
-
-HTTP_PROXY_HOST = None
-HTTP_PROXY_PORT = None
-
-DOMOTICZ_USER = ''
-DOMOTICZ_PASS = ''
-DOMOTICZ_ADDRESS = 'http://192.168.0.1:8080'
-
-#CONFIG END
 
 class Domoticz:
-
-
+    
+    
     def __init__(self, address, user, password):
         self.address = address
         self.user = user
-        self.password = password
-        DomoticzDevice = namedtuple("DomoticzDevice", "name idx type")
-        devicesAndScenes = []
+        self.password = password       
+        self.devicesAndScenes = []
 
-    def __populateUsingURL(url, deviceType):
-        requestUrl = DOMOTICZ_ADDRESS + url
-        response = requests.get(requestUrl, auth=(user, password))
+    def __populateUsingURL(self, url, deviceType):
+        requestUrl = self.address + url
+        print("RequestUrl: " + requestUrl)
+        response = requests.get(requestUrl, auth=(self.user,self. password))
         if response.status_code != 200:
             print("Bad Request")
             return None
@@ -39,23 +30,23 @@ class Domoticz:
             devices = jsonDevices["result"]
             for device in devices:
                 tempDevice = DomoticzDevice(device["Name"], device["idx"], deviceType)
-                devicesAndScenes.append(tempDevice)
+                self.devicesAndScenes.append(tempDevice)
 
-    def __populateDevicesAndScenes():
-        devicesAndScenes = []
+    def __populateDevicesAndScenes(self):
+        self.devicesAndScenes = []
         getAllSwitches = "/json.htm?type=command&param=getlightswitches"
-        populateUsingURL(getAllSwitches, 0)
+        self.__populateUsingURL(getAllSwitches, 0)
         getAllScenes = "/json.htm?type=scenes"
-        populateUsingURL(getAllScenes, 1)
+        self.__populateUsingURL(getAllScenes, 1)
 
-    def __doesDeviceExist(deviceName):
+    def __doesDeviceExist(self, deviceName):
         deviceNameToTest = deviceName.lower()
-        for device in devicesAndScenes:
+        for device in self.devicesAndScenes:
             if device[0].lower() == deviceNameToTest :
                 return device
         return None
 
-    def __getTargetDevice(words):
+    def __getTargetDevice(self, words):
         print("finding device...")
         wordsLength = len(words)
         targetDevice = ""
@@ -63,7 +54,7 @@ class Domoticz:
             if i > 0 :
                 targetDevice += " "
             targetDevice += words[i + 2]
-            returnDevice = doesDeviceExist(targetDevice)
+            returnDevice = self.__doesDeviceExist(targetDevice)
             if returnDevice != None :
                 return returnDevice
 
@@ -71,11 +62,11 @@ class Domoticz:
         return None # No matches
 
 
-    def __sendCommand(command, deviceId, deviceType):
+    def __sendCommand(self, command, deviceId, deviceType):
         # e.g. '/json.htm?type=command&param=switchscene&idx=1'
         url = ""
         param = ""
-
+            
         if deviceType == 0:
             param = "switchlight"
         elif deviceType == 1:
@@ -83,18 +74,20 @@ class Domoticz:
 
         jsonString = '/json.htm?type=command&'
         switchCommand = '&switchcmd='
-        seq = (address, jsonString, "param=", param, "&idx=", deviceId, switchCommand, command)
+        seq = (self.address, jsonString, "param=", param, "&idx=", deviceId, switchCommand, command)
         blankString = ''
         url = blankString.join(seq)
 
         print(url, '\n')
-        response = requests.get(url, auth=(user, password))
+        response = requests.get(url, auth=(self.user, self.password))
         if response.status_code != 200:
             print("Bad Send Request")
             return None
+                
 
-
-    def ProcessCommand(message):
+    def ProcessCommand(self, message):
+        print("Processing Domoticz Command")
+        self.__populateDevicesAndScenes()
         lines = message.split('\n')
         commandUnderstood = False
         for line in lines:
@@ -102,19 +95,41 @@ class Domoticz:
             if len(words) > 2:
                 if words[0] == '#command':
                     commandUnderstood = True
-                    sendCommand(words[1], words[2], 0)
+                    self.__sendCommand(words[1], words[2], 0)
                 elif words[0] == '#commandToScene':
                     commandUnderstood = True
-                    sendCommand(words[1], words[2], 1)
+                    self.__sendCommand(words[1], words[2], 1)
                 elif words[0] == '#commandByName':
                     print("Processing Command by Name")
                     commandUnderstood = True
-                    targetDevice = getTargetDevice(words)
+                    targetDevice = self.__getTargetDevice(words)
                     if targetDevice != None:
                         print("Target Device is " + targetDevice[0])
-                        sendCommand(words[1], targetDevice[1], targetDevice[2])
+                        self.__sendCommand(words[1], targetDevice[1], targetDevice[2])
                     else:
                         print("Cannot find device: ")
-
+            
         return commandUnderstood
 
+
+    def PrintObject(self):
+        print("\nAddress: " + self.address)
+        print("\nUsername: " + self.user)
+        print("\nPassword: " + self.password)
+
+
+
+def main():
+    domoticz = Domoticz("http://192.168.0.1:8080", "", "")
+    #domoticz.PrintObject()
+    while True:
+        userInput = input("Please enter a Command: \n")
+        domoticz.ProcessCommand(userInput)
+    #domoticz.ProcessCommand("#commandByName On lamp")    
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
