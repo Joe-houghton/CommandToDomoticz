65ea3b8761b0840e85eca6324a2b23d4c798c135
Joe-houghton <Joe_houghton@live.co.uk>
2018-07-05 21:37:58 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CommandToDomoticz",
+    version="0.9.0",
+    author="Joe Houghton",
+    author_email="github_joe_Houghton@outlook.com",
+    description="Control Domoticz via Commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Joe-houghton/CommandToDomoticz",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    
+    install_requires=['logging', 'json', 'requests'],
+)
